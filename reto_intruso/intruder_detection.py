825f63fc891d1d2bfc4c8f0d6596afecb62eb5b5
Gabo1245 <ponce.pgabriel@gmail.com>
2023-03-13 22:34:21 -0600

    Final Version Finished

--- reto_intruso/intruder_detection.py
+++ reto_intruso/intruder_detection.py
@@ -6,7 +6,7 @@ import pygame
 
 Contour_Area = 4000
 History = 500
-
+Dist2Threshold = 400
 
 def alarma(cap, mov):
     
@@ -23,7 +23,7 @@ def alarma(cap, mov):
     #Si no se lee el video bien, se termina el programa
     if not ret:
         print("CAMERA PROBLEM")
-        return
+        quit()
     
     #Aplicamos detector
     mascara = mov.apply(frame)
@@ -47,6 +47,7 @@ def alarma(cap, mov):
             
             #Dibujo del rectangulo
             cv2.rectangle(frame, (x,y), (x+w, y+h),(0,0,255),2)
+            cv2.rectangle(contornos, (x,y), (x+w, y+h),(0,0,255),2)
             cv2.putText(frame, '{}'.format("Alarma"),(x,y-5),1,1.3,(0,0,255),1,cv2.LINE_AA)
         #Mostramos la camara, mascara y contornos
         cv2.imshow("Alarma activa", frame)
@@ -54,58 +55,35 @@ def alarma(cap, mov):
         cv2.imshow("Contornos", contornos)
         if cv2.waitKey(20) & 0xFF==ord('p'):
             print("Finished")
-            return
-    #cap.release()
-    #cv2.destroyAllWindows()
-
-def normal():
-    cap = cv2.VideoCapture(0)
-    mov = cv2.createBackgroundSubtractorKNN(history=500, dist2Threshold=400, detectShadows=False)
-
-    cv2.ocl.setUseOpenCL(False)
-
-    while(True):
-        ret,frame = cap.read()
-
-        if not ret:
-            break
-        
-        mascara = mov.apply(frame)
-
-        contornos = mascara.copy()
-
-        con, jerarquia = cv2.findContours(contornos, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        for c in con:
-            if cv2.contourArea(c) < 1500:
-                continue
-            if cv2.contourArea(c) >= 1500:
-                (x,y,w,h) = cv2.boundingRect(c)
-
-                cv2.rectangle(frame, (x,y), (x+w, y+h),(0,0,255),2)
-                cv2.putText(frame, '{}'.format("Alarma"),(x,y-5),1,1.3,(0,0,255),1,cv2.LINE_AA)
-
-            cv2.imshow("Alarma Inactiva", frame)
-            cv2.imshow("Umbral", mascara)
-            cv2.imshow("Contornos", contornos)
-
-            k = cv2.waitKey(5)
-            if k ==  27:
-                break
-
-        #cap.release()
-        #cv2.destroyAllWindows()
+            quit()
 
 
 
-def pantalla_principal():
-    (width, height) = (1900, 400)
-    screen = pygame.display.set_mode((width, height))
+
+def pantalla_principal(screen):
+
     pygame.display.flip()
-    button = [[(255,255,255), [250, 20], [65, 40], "power", "shortcuts"]]
+    buttons = [[(255,0,0), [0, 20], [65, 200], 'Contour_Area', -250],
+               [(0,255,0), [70, 20], [65, 200], 'Contour_Area', 250],]
+    mouse = pygame.mouse.get_pos()
+
+    conarstr = f'Contour Area: {Contour_Area}'
+    historystr = f'History: {History}'
+    dist2thresstr = f'Distance To Threshold: {Dist2Threshold}'
+
     for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] and self._rect.collidepoint(self.mouse):
-            pass
+        for color, coors, rect, var, val in buttons:
+            #print(color)
+            surface = pygame.Surface(rect)
+            surface.fill(color)
+            _rect = screen.blit(surface, coors)
+                
+            if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] and _rect.collidepoint(mouse):
+                #eval(f'{var}+={val}')
+                globals()[var] += val
+                print(globals()[var])
+
+
 def main():
     #Creamos el video
     cap = cv2.VideoCapture(0)
@@ -115,12 +93,16 @@ def main():
     cap.set(cv2.CAP_PROP_FOURCC, 0x32595559)
     cap.set(cv2.CAP_PROP_FPS, 25)
 
-    #Llamada al detector de video
-    mov = cv2.createBackgroundSubtractorKNN(history=History, dist2Threshold=400, detectShadows=False)
 
+    (width, height) = (300, 300)
+    screen = pygame.display.set_mode((width, height))
+
+        #Llamada al detector de video
+    
+    mov = cv2.createBackgroundSubtractorKNN(history=History, dist2Threshold=Dist2Threshold, detectShadows=False)
     while True:    
         alarma(cap, mov)
-        pantalla_principal()   
+        pantalla_principal(screen)   
 
 main()
 
